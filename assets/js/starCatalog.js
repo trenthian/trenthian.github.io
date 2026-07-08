ad34a3d16406dc8db9b712d08a16634cf1d464a2
Starry Stoat <trenthian@gmail.com>
2026-07-08 09:41:52 -0700

    Create starCatalog.js

---
+++ assets/js/starCatalog.js
@@ -0,0 +1,12 @@
+/*
+==================================================
+ Stellar Stoat Portfolio
+ Version 2.3
+
+ starCatalog.js
+
+ SSoT for every project displayed on the website.
+
+ Rendering logic lives elsewhere.
+==================================================
+*/
